803aa6b0ce9e7a5101b39a3b0b4cc0cffe47c813
Suzuki Takafumi <suzukitakafumifcc@gmail.com>
2024-04-04 12:41:55 +0900

    feat: completed chapter 15

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -113,6 +113,13 @@ export async function authenticate(
     await signIn('credentials', formData);
   } catch (error) {
     if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
     }
+    throw error;
   }
 }
--- auth.ts
+++ auth.ts
@@ -1,14 +1,14 @@
 import NextAuth from 'next-auth';
-import { authConfig } from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
-import { z } from 'zod';
+import { authConfig } from './auth.config';
 import { sql } from '@vercel/postgres';
+import { z } from 'zod';
 import type { User } from '@/app/lib/definitions';
 import bcrypt from 'bcrypt';
 
 async function getUser(email: string): Promise<User | undefined> {
   try {
-    const user = await sql<User>`SELECT * FROM users WHERE email${email}`;
+    const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
     return user.rows[0];
   } catch (error) {
     console.error('Failed to fetch user:', error);
@@ -29,11 +29,12 @@ export const { auth, signIn, signOut } = NextAuth({
           const { email, password } = parsedCredentials.data;
           const user = await getUser(email);
           if (!user) return null;
-          const passwordMatch = await bcrypt.compare(password, user.password);
-
-          if (passwordMatch) return user;
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+ 
+          if (passwordsMatch) return user;
         }
 
+        console.log('Invalid credentials');
         return null;
       },
     }),
